d99ad269612c7cee457533f663d3df4d6c403097
Alireza5928 <alirezaabedzahe@gmail.com>
2016-12-13 16:08:34 +0330

    Update config.lua

--- config.lua
+++ config.lua
@@ -1,4 +1,4 @@
-bot_token = "TOKEN"
+bot_token = "305116342:AAF40Q1S02xju4Ekl4GdLHk8njttF35V-IQ"
 send_api = "https://api.telegram.org/bot"..bot_token
 bot_version = "1.0"
-admingp = -1078684925
+admingp = -1081247483
